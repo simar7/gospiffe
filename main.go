43bee55524654693d8a704802afcca9bed4a3bf4
Simarpreet Singh <simar@linux.com>
2019-03-22 15:17:11 -0700

    x.509: add basic snippets with chi router
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- main.go
+++ main.go
@@ -1,1 +1,50 @@
-package gospiffe
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+type Server struct {
+	Addr string
+}
+
+func (s *Server) NewRouter() *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/x509", FetchX509SVID)
+
+	return r
+}
+
+func FetchJWTSVID(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+	w.Write([]byte("FetchJWTSVID"))
+}
+func ValidateJWTSVID(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+	w.Write([]byte("ValidateJWTSVID"))
+}
+
+func FetchJWTBundles(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+	w.Write([]byte("FetchJWTBundles"))
+}
+
+func FetchX509SVID(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+	w.Write([]byte("FetchX509SVID"))
+}
+
+func main() {
+	s := &Server{Addr: ":9991"}
+	r := s.NewRouter()
+	http.ListenAndServe(s.Addr, r)
+}
--- main_test.go
+++ main_test.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_FetchX509SVID(t *testing.T) {
+	rr := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodGet, "/x509", nil)
+	assert.NoError(t, err)
+
+	s := Server{}
+	s.NewRouter().ServeHTTP(rr, req)
+
+	assert.NotNil(t, req)
+
+	resp, err := ioutil.ReadAll(rr.Body)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "FetchX509SVID", string(resp))
+	assert.Equal(t, http.StatusNotImplemented, rr.Code)
+}
